74bf053f7811ed4d782336faae08aad3b871b6a7
Hammad <muhammadhammad1487@gmail.com>
2025-03-09 03:40:19 +0500

    connected with supabase

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,11 +1,13 @@
 import { DataSource } from "typeorm";
 import { Transaction } from "./entities/Transaction";
+import dotenv from "dotenv";
 
+dotenv.config();
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || "5432"),
-  username: process.env.DB_USERNAME || "",
+  username: process.env.DB_USER || "",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "",
   synchronize: true,
