c6b9f583a43edd717f20c69d6535fa3b767daba4
Hammad <muhammadhammad1487@gmail.com>
2025-03-09 05:24:49 +0500

    get txn by id endpoint implemented

--- backend/src/routes/transaction.routes.ts
+++ backend/src/routes/transaction.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { getAllTransactions } from '../controllers/TransactionController';
+import { getAllTransactions, getTransactionById } from '../controllers/TransactionController';
 
 const router = Router();
 
 router.get('/', getAllTransactions);
+router.get('/:id', getTransactionById);
 
 export default router;
